20fd676fdf36633db3a505b31e3f6638e5d295b7
ishrathnayeem <ishrathnayeem1@gmail.com>
2020-05-17 16:44:18 -0400

    Adjustments
    Realligned

--- project/src/main/scala/Main.scala
+++ project/src/main/scala/Main.scala
@@ -14,15 +14,15 @@ object Main extends App with Config{
   stmt execute """DROP TABLE IF EXISTS ext_calendar_dates"""
   stmt execute """DROP TABLE IF EXISTS enriched_trip"""
   stmt execute """CREATE EXTERNAL TABLE fall2019_ishrath.ext_trips (
-                  |route_id    INT,
-                  |service_id  STRING,
-                  |trip_id STRING,
-                  |trip_headsign   STRING,
-                  |direction_id    STRING,
-                  |shape_id    STRING,
-                  |wheelchair_accessible   STRING,
-                  |note_fr STRING,
-                  |note_en STRING
+                  |route_id               INT,
+                  |service_id             STRING,
+                  |trip_id                STRING,
+                  |trip_headsign          STRING,
+                  |direction_id           STRING,
+                  |shape_id               STRING,
+                  |wheelchair_accessible  STRING,
+                  |note_fr                STRING,
+                  |note_en                STRING
                   |)
                   |ROW FORMAT DELIMITED
                   |FIELDS TERMINATED BY ','
@@ -33,9 +33,9 @@ object Main extends App with Config{
                   |"serialization.null.format" = "")""".stripMargin
 
   stmt execute """CREATE EXTERNAL TABLE fall2019_ishrath.ext_calendar_dates (
-                  |service_id  STRING,
-                  |date    INT,
-                  |exception_type  INT
+                  |service_id       STRING,
+                  |date             INT,
+                  |exception_type   INT
                   |)
                   |ROW FORMAT DELIMITED
                   |FIELDS TERMINATED BY ','
@@ -46,32 +46,32 @@ object Main extends App with Config{
                   |"serialization.null.format" = "")""".stripMargin
 
   stmt execute """CREATE EXTERNAL TABLE fall2019_ishrath.ext_frequencies (
-                  |    trip_id STRING,
-                  |    start_time  STRING,
-                  |    end_time    STRING,
-                  |    headway_secs    INT
-                  |  )
-                  |  ROW FORMAT DELIMITED
-                  |  FIELDS TERMINATED BY ','
-                  |  STORED AS TEXTFILE
-                  |  LOCATION '/user/fall2019/ishrath/project4/frequencies'
-                  |  TBLPROPERTIES (
-                  |    "skip.header.line.count" = "1",
-                  |    "serialization.null.format" = "")""".stripMargin
+                  |trip_id        STRING,
+                  |start_time     STRING,
+                  |end_time       STRING,
+                  |headway_secs   INT
+                  |)
+                  |ROW FORMAT DELIMITED
+                  |FIELDS TERMINATED BY ','
+                  |STORED AS TEXTFILE
+                  |LOCATION '/user/fall2019/ishrath/project4/frequencies'
+                  |TBLPROPERTIES (
+                  |"skip.header.line.count" = "1",
+                  |"serialization.null.format" = "")""".stripMargin
 
   stmt execute """SET hive.exec.dynamic.partition=true"""
   stmt execute """SET hive.exec.dynamic.partition.mode=nonstrict"""
 
   stmt execute """CREATE TABLE fall2019_ishrath.enriched_trip (
-                  |route_id                STRING,
-                  |service_id	            STRING,
-                  |trip_id	                STRING,
+                  |route_id             STRING,
+                  |service_id	          STRING,
+                  |trip_id	            STRING,
                   |trip_headsign       	STRING,
                   |direction_id        	INT,
                   |shape_id	            INT,
                   |note_fr             	STRING,
                   |note_en             	STRING,
-                  |start_time	            STRING,
+                  |start_time	          STRING,
                   |end_time	            STRING,
                   |headway_secs        	INT,
                   |date	                INT,
